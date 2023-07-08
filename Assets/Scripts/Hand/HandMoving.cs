d03a4cc29672dad519331e298d2597aae4a958b8
paledust <paledust@hotmail.com>
2023-07-08 22:11:33 +0800

    update audio

--- Assets/Scripts/Hand/HandMoving.cs
+++ Assets/Scripts/Hand/HandMoving.cs
@@ -10,6 +10,10 @@ public class HandMoving : MonoBehaviour
     [SerializeField] private Transform robotTarget_Trans;
     [SerializeField] private Transform robotHide_Trans;
     [SerializeField, ShowOnly] private HAND_MOVING_STATE handMoveState = HAND_MOVING_STATE.MANUAL;
+[Header("Audiosource")]
+    [SerializeField, Range(0,1)] private float maxVolume = 0.5f;
+    [SerializeField] private AudioSource sfx_audio;
+    [SerializeField] private AudioClip moveClip;
     public float lerpSpeed = 5;
     private Camera playerCam;
     public Vector2 positionOffset{get; private set;}
@@ -24,6 +28,7 @@ public class HandMoving : MonoBehaviour
         resetPosition.z = robotTarget_Trans.position.z;
 
         tipToHandOffset = hand_trans.position - robotTip_Trans.position;
+        sfx_audio.clip = moveClip;
     }
     void Update(){
         switch(handMoveState){
@@ -42,6 +47,13 @@ public class HandMoving : MonoBehaviour
 
         robotTarget_Trans.position = Vector3.Lerp(robotTarget_Trans.position, targetPosition, Time.deltaTime * lerpSpeed);
         positionOffset = targetPosition - robotTarget_Trans.position;
+        if(positionOffset.magnitude>1f){
+            if(!sfx_audio.isPlaying) sfx_audio.Play();
+            sfx_audio.volume = Mathf.Lerp(sfx_audio.volume, maxVolume, Time.deltaTime * 5);
+        }
+        else{
+            sfx_audio.volume = Mathf.Lerp(sfx_audio.volume, 0, Time.deltaTime * 5);
+        }
     }
     public void SwitchHandState(HAND_MOVING_STATE moveState)=>handMoveState = moveState;
 }

e2856e3fc6989bed645823611368596ca3cdd8e1
paledust <paledust@hotmail.com>
2023-07-08 23:34:49 +0800

    update CD Rotate

--- Assets/Scripts/Interaction/CD_Rotation.cs
+++ Assets/Scripts/Interaction/CD_Rotation.cs
@@ -5,22 +5,43 @@ using UnityEngine;
 public class CD_Rotation : BasicPointAndClickInteractable
 {
     [SerializeField, ShowOnly] private CD_STATE cd_state = CD_STATE.IDLE;
+    [SerializeField] private float state_transitionTime = 0.25f;
+    [SerializeField] private float state_transitionSpeed = 5f;
     private bool isControlling = false;
+    private float angluarSpeed = 0;
+    private float transitionTimer = 0;
     private Vector3 grabPoint;
     void Update(){
         switch(cd_state){
             case CD_STATE.IDLE:
                 if(isControlling){
+                    transitionTimer = 0;
                     Vector3 grabPointDir = transform.TransformPoint(grabPoint) - transform.position;
                     grabPointDir.z = 0;
                     Vector3 handtipDir = PointClick_InteractableHandler.tipPos - transform.position;
                     handtipDir.z = 0;
 
                     float angle = Vector3.SignedAngle(grabPointDir, handtipDir, transform.forward);
+                    angluarSpeed = angle/Time.deltaTime;
                     transform.rotation *= Quaternion.Euler(0,0,angle);
+                }
+                else{
+                    transform.rotation *= Quaternion.Euler(0,0,angluarSpeed*Time.deltaTime);
+
+                    if(Mathf.Abs(angluarSpeed)>state_transitionSpeed) {
+                        transitionTimer += Time.deltaTime;
+                        if(transitionTimer>state_transitionTime){
+                            Debug.Log(angluarSpeed);
+                            cd_state = CD_STATE.ROTATING;
+                        }
+                    }
+                    else{
+                        transitionTimer = 0;
+                    }
                 }
                 break;
             case CD_STATE.ROTATING:
+                transform.rotation *= Quaternion.Euler(0,0,angluarSpeed*Time.deltaTime);
                 break;
         }
     }
@@ -42,10 +63,5 @@ public class CD_Rotation : BasicPointAndClickInteractable
     {
         base.OnRelease(interactableHandler);
         isControlling = false;
-    }
-    void OnDrawGizmosSelected(){
-        Gizmos.matrix = transform.localToWorldMatrix;
-        Gizmos.color  = Color.red;
-        Gizmos.DrawSphere(grabPoint, 0.2f);
     }
 }
